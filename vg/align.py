b94e2e04eecd472253fd3ece78b27b6defff7ac9
Grzegorz Chrupala <g.chrupala@uvt.nl>
2019-05-17 14:41:14 +0200

    Added vg/flickr8k_provider.py Added vg.align.phoneme_data

--- vg/align.py
+++ vg/align.py
@@ -2,10 +2,12 @@ import json
 import multiprocessing
 nthreads = multiprocessing.cpu_count()
 import logging
-import gentle
-resources = gentle.Resources()
+
 import vg.activations
 import numpy as np
+
+
+resources = None
 
 def from_audio(modelpath, audiopaths, transcripts):
     # Align audio files
@@ -13,21 +15,22 @@ def from_audio(modelpath, audiopaths, transcripts):
     # Get activations
     activations = vg.activations.from_audio(modelpath, audiopaths)
     # Return data
+    return phoneme_activations(activations, alignments)
+
+def phoneme_activations(activations, alignments, mfcc=False):
+    """Return array of phoneme labels and array of corresponding mean-pooled activation states."""
     labels = []
     states = []
-    
+    index = (lambda ms: ms//10) if mfcc else vg.activations.index
     for activation, alignment in zip(activations, alignments):
         # extract phoneme labels and activations for current utterance
-        y, X = phoneme_activations(activation, alignment)
+        y, X = zip(*list(slices(alignment, activation, index=index)))
+        y = np.array(y)
+        X = np.stack(X)
         labels.append(y)
         states.append(X)
     return np.concatenate(labels), np.concatenate(states)
 
-    
-def phoneme_activations(activations, alignment):
-    """Return array of phoneme labels and array of corresponding mean-pooled activation states."""
-    labels, states = zip(*list(slices(alignment, activations, index=vg.activations.index)))
-    return np.array(labels), np.stack(states)
     
 def on_progress(p):
     for k,v in p.items():
@@ -35,12 +38,16 @@ def on_progress(p):
 
 
 def align(audiopath, transcript):
+    import gentle
+    global resources
+    if resources is None:
+        resources = gentle.Resources()
     logging.info("converting audio to 8K sampled wav")
     with gentle.resampled(audiopath) as wavfile:
         logging.info("starting alignment")
         aligner = gentle.ForcedAligner(resources, transcript, nthreads=nthreads, disfluency=False, 
                                    conservative=False)
-        return json.loads(aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging).to_json())
+        return json.loads(aliger.transcribe(wavfile, progress_cb=on_progress, logging=logging).to_json())
 
 
 def slices(utt, rep, index=lambda ms: ms//10, aggregate=lambda x: x.mean(axis=0)):
@@ -68,3 +75,37 @@ def phones(utt):
             label = phone['phone'].split('_')[0]
             if label != 'oov':
                 yield (label, int(start*1000), int(end*1000))
+
+def phoneme_data(nets, alignment_path="./data/flickr8k/dataset.val.fa.json", batch_size=64):
+    """Generate data for training a phoneme decoding model."""
+    import vg.flickr8k_provider as dp
+    logging.getLogger().setLevel('INFO')
+    logging.info("Loading alignments")
+    data = {}
+    for line in open(alignment_path):
+        item = json.loads(line)
+        data[item['sentid']] = item
+    logging.info("Loading audio features")
+    prov = dp.getDataProvider('flickr8k', root='.', audio_kind='mfcc')
+    val = list(prov.iterSentences(split='val'))
+    alignments_all = [ data[sent['sentid']] for sent in val ]
+    alignments = [ item for item in alignments_all if np.all([word.get('start', False) for word in item['words']]) ]
+    sentids = set(item['sentid'] for item in alignments)
+    audio = [ sent['audio'] for sent in val if sent['sentid'] in sentids ]
+    result = {}
+    logging.info("Computing data for MFCC")
+    y, X = phoneme_activations(audio, alignments, mfcc=True)
+    result['mfcc'] = fa_data(y, X)
+    for name, net in nets:
+        logging.info("Computing data for {}".format(name))
+        activations = vg.activations.get_state_stack(net, audio, batch_size=batch_size)
+        y, X = phoneme_activations(activations, alignments, mfcc=False)
+        result[name] = fa_data(y, X)
+    return result
+
+def fa_data(y, X):
+    # Get rid of NaNs
+    # ix = np.isnan(X.sum(axis=1))
+    # X = X[~ix]
+    #y = y[~ix]
+    return dict(features=X, labels=y)
